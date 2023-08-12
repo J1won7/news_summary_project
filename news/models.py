91405b9dd6f16aa676b5af39e0e9160799c6b234
alalalek <alalalek@naver.com>
2023-08-12 23:44:26 +0900

    None

--- news/models.py
+++ news/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 
 class News(models.Model):
-    newsid = models.IntegerField(primary_key=True)
+    _id = models.IntegerField(primary_key=True)
     category = models.IntegerField()
     title = models.CharField(max_length=50)
-    image = models.CharField(max_length=200)
+    image = models.FilePathField(null=True)
     summary = models.TextField()
     write_time = models.CharField(max_length=20)
-    url = models.CharField(max_length=50)
+    url = models.FilePathField()
+
+    class Meta:
+        db_table = "news"
