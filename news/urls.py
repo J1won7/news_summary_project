af4f9584986d6a23df2e3f99a8e638f8d9483c77
alalalek <alalalek@naver.com>
2023-08-14 21:54:57 +0900

    None

--- news/urls.py
+++ news/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.news_objects, name='news_list'),
-    #path('photo/<int:pk>/', views.news_detail, name='news_detail'),
-    #path('photo/<int:pk>/edit/', views.news_edit, name='news_edit'),
+    path('<int:category>', views.news_objects, name='news_list_by_category'),
 ]
--- news/views.py
+++ news/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import News
 
 
-def news_objects(request):
-    news_objects = News.objects.order_by('-time')[:20]
+def news_objects(request, category=100):
+    news_objects = News.objects.filter(category=category).order_by('-time')[:20]
     return render(request, 'news/news_list.html', {'news_objects': news_objects})
